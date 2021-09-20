468a7be21d13d78e6906099b1248d644a7dc889d
Freilla Mae Espinola <freillamae13@gmail.com>
2021-09-20 19:26:43 +0800

    Updated

--- wp-content/themes/freia/inc/components/header.php
+++ wp-content/themes/freia/inc/components/header.php
@@ -1,63 +1,33 @@
 <?php
 	$header_logo = acf_get_image( get_field('header_logo', 'option') );
-
-	$header_cta = [
-		'style' => acf_get_btn_style( get_field('header_cta_link_style', 'option') ),
-		'image' => acf_get_image( get_field('header_cta_link_image', 'option') ),
-		'link' => acf_get_link( get_field('header_cta_link_type', 'option'), get_field('header_cta_custom_link', 'option'), get_field('header_cta_page_link', 'option') ),
-		'text' => get_field('header_cta_link_label', 'option'),
-		'tab' => acf_open_in_new_tab( get_field('header_cta_open_in_new_tab', 'option') )
-	];
 ?>
 
 <header class="header">
 	<div class="grid-x grid-container align-center">
-		<div class="cell small-11 large-10">
+		<div class="cell small-11 large-12">
 			<div class="grid-x align-middle">
-				<div class="header__logo cell auto xlarge-shrink">
+				<nav class="header__menu cell large-5 text-right">
+					<ul>
+						<li><a href="#" class="animsition-link">Home</a></li>
+						<li><a href="#" class="animsition-link">Explore</a></li>
+					</ul>
+				</nav>
+
+				<div class="header__logo cell auto text-center">
 					<a href="<?= home_url(); ?>">
 						<picture>
-							
 							<source media="(min-width: 1024px)" data-srcset="<?= $header_logo['sizes']['image_size_319']; ?>">
 							<img class="lazy" data-src="<?= $header_logo['url']; ?>" alt="<?= $header_logo['alt']; ?>">
 						</picture>
 					</a>
 				</div>
 
-				<nav class="header__menu cell auto text-right">
-					<ul>
-						<?php 
-							wp_nav_menu([
-								'menu_class' => '',
-								'container' => '',
-								'items_wrap' => '%3$s',
-								'theme_location' => 'header_nav',
-							]); 
-						?>
+				<nav class="header__menu cell large-5 text-left">
+				<ul>
+					<li><a href="#" class="animsition-link">Brands</a></li>
+					<li><a href="#" class="animsition-link">Login</a></li>
+				</ul>
 
-						<?php if ( $header_cta['text'] || $header_cta['image'] ) : ?>
-							<li class="header__menu-cta">
-								<a href="<?= $header_cta['link']; ?>" <?= $header_cta['tab']; ?> class="<?= $header_cta['style']; ?>">
-									<?php if ( $header_cta['style'] == 'image' ) : ?>
-										<img src="<?= $header_cta['image']['url']; ?>" alt="<?= $header_cta['image']['alt']; ?>">
-									<?php
-										else : 
-											echo $header_cta['text'];
-										endif; 
-									?>
-								</a>
-							</li>
-						<?php endif; ?>
-					</ul>
-				</nav>
-
-				<div class="header__hamburger cell shrink text-right">
-					<div>
-						<div></div>
-						<div></div>
-						<div></div>
-					</div>
-				</div>
 			</div>
 		</div>
 	</div>
---
+++ wp-content/themes/freia/page-templates/page-home.php
@@ -0,0 +1,64 @@
+<?php
+	/* Template Name: Homepage */
+	get_header();
+?>
+<div class="flexspace desktop" style="margin-top: 10rem;"></div>
+<div class="flexspace tablet" style="margin-top: 5rem;"></div>
+<div class="flexspace mobile" style="margin-top: 5rem;"></div>
+
+<section class="banner">
+	<div class="banner__full banner__full-center">
+		<div class="banner__full-bgImg" style="background-image:url(http://startctrlstockphotos.mae/wp-content/uploads/2021/09/cropped-banner-people.png);">
+			<div class="banner__full-grid grid-x align-center align-middle grid-container">
+				<div class="banner__full-content text-center cell small-12 large-12">
+					<h1>startctrl</h1>
+					<p>Curated Resources to Energize Your Creative Workflow.</p>
+					<div class="banner__full-cta">
+						<a class="button button__default" href="#">View Curated Photos</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+
+<div class="flexspace desktop" style="margin-top: 10rem;"></div>
+<div class="flexspace tablet" style="margin-top: 5rem;"></div>
+<div class="flexspace mobile" style="margin-top: 5rem;"></div>
+
+<section class="intro-content">
+	<div class="grid-x align-center align-middle grid-container">
+		<p>Explore diverse collections of our most incredible high-resolution, royalty-free, stock assets. Powered by creators everywhere.</p>
+	</div>
+</section>
+
+<div class="flexspace desktop" style="margin-top: 10rem;"></div>
+<div class="flexspace tablet" style="margin-top: 5rem;"></div>
+<div class="flexspace mobile" style="margin-top: 5rem;"></div>
+
+<section class="column-content">
+	<div class="column-content__grid grid-x align-center align-middle grid-container">
+		<div class="column-content__columns cell large-12 medium-12 small-12">
+			<div class="grid-x">
+				<div class="column-content__image cell large-6 medium-6 small-12">
+					<img class="lazy entered loaded" data-src="https://www.revamp.bryanalano.com/wp-content/uploads/2015/12/Summer-Hiking_3.jpg" alt="1 g9s9ufXZIe8erjV842ufoA" data-ll-status="loaded" src="https://www.revamp.bryanalano.com/wp-content/uploads/2015/12/Summer-Hiking_3.jpg">
+				</div>
+				<div class="column-content__image cell large-6 medium-6 small-12">
+					<img class="lazy entered loaded" data-src="https://www.revamp.bryanalano.com/wp-content/uploads/2018/07/I4P7037-1.jpg" alt="1 fMplyq5kz5Fsmh9l0hEZAg" data-ll-status="loaded" src="https://www.revamp.bryanalano.com/wp-content/uploads/2018/07/I4P7037-1.jpg">
+				</div>
+				<div class="column-content__image cell large-6 medium-6 small-12">
+					<img class="lazy entered loaded" data-src="https://www.revamp.bryanalano.com/wp-content/uploads/2018/06/LU_PARKER_ABC_NEWS_ANCHOR_BRYANALANO.jpg" alt="1 g9s9ufXZIe8erjV842ufoA" data-ll-status="loaded" src="https://www.revamp.bryanalano.com/wp-content/uploads/2018/06/LU_PARKER_ABC_NEWS_ANCHOR_BRYANALANO.jpg">
+				</div>
+				<div class="column-content__image cell large-6 medium-6 small-12">
+					<img class="lazy entered loaded" data-src="https://www.revamp.bryanalano.com/wp-content/uploads/2015/04/BryanAlano_Lifestyle_kids_fun_outdoor_birthday11.jpg" alt="1 fMplyq5kz5Fsmh9l0hEZAg" data-ll-status="loaded" src="https://www.revamp.bryanalano.com/wp-content/uploads/2015/04/BryanAlano_Lifestyle_kids_fun_outdoor_birthday11.jpg">
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+
+<div class="flexspace desktop" style="margin-top: 10rem;"></div>
+<div class="flexspace tablet" style="margin-top: 5rem;"></div>
+<div class="flexspace mobile" style="margin-top: 5rem;"></div>
+
+<?php get_footer();
